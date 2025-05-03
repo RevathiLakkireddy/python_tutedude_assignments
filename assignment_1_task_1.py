d876ee688b37f418841be741a6e99a574aea265d
RevathiLakkireddy <revathilakkireddy6@gmail.com>
2025-05-03 20:18:54 +0530

    Add files via upload

---
+++ assignment_1_task_1.py
@@ -0,0 +1,20 @@
+
+#Task1- perform basic mathematical operation
+
+#initialize variables and taking inputs from the user
+first_num = int(input("Enter the first number: "))
+second_num = int(input("Enter the second number: "))
+
+#performing mathematical operations
+addition = first_num + second_num
+subtraction = first_num - second_num
+multiplication = first_num * second_num
+division = first_num / second_num
+
+
+#displaying the result
+print(f"addition: {addition}")
+print(f"subtraction: {subtraction}")
+print(f"multiplication: {multiplication}")
+print(f"division: {division}")
+
---
+++ assignment_1_task_2.py
@@ -0,0 +1,8 @@
+#Task2- creating a personalized greeting
+
+#initializing variables and taking inputs from the user to store their first name & last name
+first_name = input("Enter the first name: ")
+last_name = input("Enter the last name: ")
+
+#displaying the personalized greeting
+print(f"Hello, {first_name} {last_name}! welcome to the python program.")
